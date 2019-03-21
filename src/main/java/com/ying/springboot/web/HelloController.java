e38dc562410d0694072b6147c9ee864c241d6cc7
wufazhuceya <1379370167@qq.com>
2019-03-21 17:11:15 +0800

    springboot

---
+++ src/main/java/com/ying/springboot/web/HelloController.java
@@ -0,0 +1,18 @@
+package com.ying.springboot.web;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+	@RequestMapping("/hello")
+	public String hello(Model m) {
+		m.addAttribute("now",DateFormat.getDateTimeInstance().format(new Date()));
+		return "";
+		
+	}
+}
